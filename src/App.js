a62ffbc0f8ddc0a8ef60d3a0e937446a4ef2d6a8
Jagjit Singh <jagjitsingh@domain7.com>
2022-03-15 23:51:17 -0700

    First Dump for all the code files.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bgimg">
+        <div className="topleft">
+        </div>
+        <div className="middle">
+          <h1>Hang tight, we are busy building your dream home.</h1>
+        </div>
+        <div className="bottomleft">
+        </div>
+      </div>
     </div>
   );
 }
